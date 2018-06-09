f0f01bfed2eb40fc26b2949ca79ecbc7b7472c1a
Lucas Valentim <lucasvalentimdias@gmail.com>
2018-06-09 00:25:02 -0300

    Substituição da função fabs da biblioteca math pela função abs e remoção dos parenteses desnecessários do while

--- metodo_de_newton.py
+++ metodo_de_newton.py
@@ -1,17 +1,14 @@
-from math import fabs
-
-
 class MetodoDeNewton(object):
     def __init__(self, f, f_derivada, x0, precisao=0.001, max_interacoes=15):
         self.__X = [x0]
         self.__interacoes = 0
         
-        while(self.interacoes < max_interacoes):        
+        while self.interacoes < max_interacoes:        
             self.__X.append(self.X[-1] - f(self.X[-1]) / f_derivada(self.X[-1]))
             
             self.__interacoes += 1
             
-            if fabs(self.X[-2] - self.X[-1]) <= precisao:
+            if abs(self.X[-2] - self.X[-1]) <= precisao:
                 break
     
     @property
